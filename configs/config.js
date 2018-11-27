347c9b82a7cd1f5f69c2df37ce40c1363db8b6f9
unknown <hacktiegr1989@gmail.com>
2018-11-27 14:39:15 +0700

    apply get method to get user info with id as param

--- configs/config.js
+++ configs/config.js
@@ -1,3 +1,11 @@
-const PORT = 5000;
+const pgp = require('pg-promise')();
 
-module.exports = PORT;
+
+const PORT = 5000;
+const DB = pgp('postgres://postgres:123@localhost:5432/postgres')
+
+
+module.exports = {
+  PORT,
+  DB
+};
--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-const PORT = require('./configs/config');
+const ENV = require('./configs/config');
+//
+const parser = require('body-parser');
 const express = require('express');
 const app = express();
+const db = ENV.DB;
+//
+app.use(parser.json());
+//
+app.get('/', (req,res) => res.send(' Connection successful !'));
+// get user info with ID
+app.get('/users/:userID', (req,res) => {
+  db.one('SELECT * from users WHERE id = $1', req.params.userID)
+  .then(function (data) {
+    return res.status('200').json(data)
+  })
+  .catch(function (error) {
+    return res.status('401').json({code:501, message:'something went wrong',error: error})
+  })
+});
 
-app.get('/', (req,res) => res.send(' Hello World !'))
-
-app.listen(PORT, () => console.log(`Example app on port ${PORT}`))
+app.listen(ENV.PORT, () => console.log(`Example app on port ${ENV.PORT}`))
