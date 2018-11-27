ff1f2eca1171504fc7f8a8bff8cc6981569a1c95
unknown <hacktiegr1989@gmail.com>
2018-11-27 17:53:35 +0700

    add put/delete/post to users

--- configs/config.js
+++ configs/config.js
@@ -4,7 +4,6 @@ const pgp = require('pg-promise')();
 const PORT = 5000;
 const DB = pgp('postgres://postgres:123@localhost:5432/postgres')
 
-
 module.exports = {
   PORT,
   DB
--- index.js
+++ index.js
@@ -4,19 +4,62 @@ const parser = require('body-parser');
 const express = require('express');
 const app = express();
 const db = ENV.DB;
-//
+// DB stuff
+// parser
 app.use(parser.json());
-//
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                        Main methods for API                                            //
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
 app.get('/', (req,res) => res.send(' Connection successful !'));
 // get user info with ID
+app.get('/users', (req, res) => {
+  db.query('SELECT * FROM users')
+    .then((data) => {
+      return res.status('200').json(data)
+    })
+    .catch(function (error) {
+      return res.status('401').json({ name:error.name, query:error.query, message:error.message,stack:error.stack })
+    })
+});
+// get user by id
 app.get('/users/:userID', (req,res) => {
-  db.one('SELECT * from users WHERE id = $1', req.params.userID)
+  db.query('SELECT * from users WHERE id = $1', req.params.userID)
   .then(function (data) {
     return res.status('200').json(data)
   })
   .catch(function (error) {
-    return res.status('401').json({code:501, message:'something went wrong',error: error})
+    return res.status('401').json({ name:error.name, query:error.query, message:error.message,stack:error.stack })
   })
 });
-
+//
+app.post('/users', (req, res) => {
+  db.query('INSERT INTO users(email, password) values($1,$2)', [req.query.email, req.query.password])
+    .then((data) => {
+      return res.status('200').json({ code: '200', message: 'Successfully added user ', data: data });
+    })
+    .catch((error) => {
+      return res.status('401').json({ name:error.name, query:error.query, message:error.message,stack:error.stack })
+    })
+});
+//
+app.put('/users/:userID', (req,res) => {
+  db.query('UPDATE users SET email=$1 WHERE id=$2 ', ['chagned@gmail.com', req.params.userID])
+    .then(() => {
+      return res.status('200').json({ code: '200', message: `Successfully updated user ${req.params.userID}` });
+    })
+    .catch((error) => {
+      return res.status('401').json({ name:error.name, query:error.query, message:error.message,stack:error.stack })
+    })
+});
+// 
+app.delete('/users/:userID', (req,res) => {
+  db.query('DELETE FROM users WHERE id = $1', req.params.userID)
+  .then(() => {
+    return res.status('200').json({ code: '200', message: `Successfully delete user with userID = ${req.params.userID}` })
+  })
+  .catch((error) => {
+    return res.status('401').json({ name:error.name, query:error.query, message:error.message,stack:error.stack })
+  })
+});
+// Ports no
 app.listen(ENV.PORT, () => console.log(`Example app on port ${ENV.PORT}`))
