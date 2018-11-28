f9ff48d1300adb1f60e26d8f4c3569292008437f
unknown <hacktiegr1989@gmail.com>
2018-11-28 15:53:43 +0700

    test separate func

--- index.js
+++ index.js
@@ -13,60 +13,30 @@ app.use(parser.json());
 //                                        Main methods for API                                            //
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 app.get('/', (req,res) => res.send(' Connection successful !'));
-// get user info with ID
-/* app.get('/users', async (req, res) => {
-  db.query('SELECT * FROM users')
-    .then((data) => {
-      return res.status('200').json({ code: '200', message: 'Successfully added user ', data: data });
-    })
-    .catch((error) => {
-      return res.status('401').json({ name:error.name, query:error.query, message:error.message,stack:error.stack })
-    })
-}); */
-// get user by id
-app.get('/users/:userID', async (req,res) => {
-  db.query('SELECT * from users WHERE id = $1', req.params.userID)
-  .then(function (data) {
-    if (data.length === 0){
-      return res.status('201').json({ message: `Cannot find user with id = ${userID}` })
-    }
-    return res.status('200').json({ code: '200', message: 'Success', data: data })
-  })
-  .catch(function (error) {
-    return res.status('401').json({ name:error.name, query:error.query, message:error.message,stack:error.stack })
-  })
-});
-/* //
-app.post('/users', (req, res) => {
-
-});
 //
-app.put('/users/:userID', (req,res) => {
-
-});
-// 
-app.delete('/users/:userID', (req,res) => {
-
-}); */
-
-app.route('/users')
-  .get((req,res) => {
-    db.query('SELECT * FROM users')
-      .then((data) => {
-        return res.status('200').json({ code: '200', message: 'Successfully added user ', data: data });
+// TEST 
+_handleGetUsers = (req, res) => {
+  return db.query('SELECT * FROM users')
+          .then((data) => {
+            return res.status('200').json({ code: '200', message: 'Successfully added user ', data: data });
+          })
+          .catch((error) => {
+            return res.status('401').json({ name:error.name, query:error.query, message:error.message,stack:error.stack })
+          })    
+};
+// Route /users/userID
+app.route('/users/:userID')
+  .get((req, res) => {
+    db.query('SELECT * from users WHERE id = $1', req.params.userID)
+      .then(function (data) {
+        if (data.length === 0){
+          return res.status('201').json({ message: `Cannot find user with id = ${userID}` })
+        }
+        return res.status('200').json({ code: '200', message: 'Success', data: data })
       })
-      .catch((error) => {
+      .catch(function (error) {
         return res.status('401').json({ name:error.name, query:error.query, message:error.message,stack:error.stack })
-      })    
-  })
-  .post((req,res) => {
-    db.query('INSERT INTO users(email, password) values($1,$2)', [req.query.email, req.query.password])
-      .then((data) => {
-        return res.status('200').json({ code: '200', message: 'Successfully added user ', data: data });
       })
-      .catch((error) => {
-        return res.status('401').json({ name:error.name, query:error.query, message:error.message,stack:error.stack })
-      })  
   })
   .put((req,res) => {
     db.query('UPDATE users SET email=$1 WHERE id=$2 ', ['chagned@gmail.com', req.params.userID])
@@ -86,5 +56,21 @@ app.route('/users')
         return res.status('401').json({ name:error.name, query:error.query, message:error.message, stack:error.stack })
       })
   })
+
+// Route /users
+app.route('/users')
+  .get((req,res) => {
+    _handleGetUsers(req, res)
+  })
+  .post((req,res) => {
+    db.query('INSERT INTO users(email, password) values($1,$2)', [req.query.email, req.query.password])
+      .then((data) => {
+        return res.status('200').json({ code: '200', message: 'Successfully added user ', data: data });
+      })
+      .catch((error) => {
+        return res.status('401').json({ name:error.name, query:error.query, message:error.message,stack:error.stack })
+      })  
+  })
+
 // Ports no
 app.listen(process.env.PORT || ENV.PORT, () => console.log(`Example app on port ${ENV.PORT}`))
