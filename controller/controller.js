c72f52b0d82481a144d0e244ee609ea4f0954c9f
unknown <hacktiegr1989@gmail.com>
2018-12-10 20:02:32 +0700

    aa

--- controller/controller.js
+++ controller/controller.js
@@ -1,8 +1,7 @@
+import { GET_ALL_USER } from './query';
 
 const pgp = require("pg-promise")();
 const db = pgp(process.env.DATABASE_URL);
-import { GET_ALL_USER } from './query'
-
 
 const handleGetUsers = (req, res) => {
   db.query(GET_ALL_USER)
